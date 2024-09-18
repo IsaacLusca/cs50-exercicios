a0d3404de85e585b516a7465fb85cfc717d09a01
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-18 20:27:11 +0000

    Wed, Sep 18, 2024, 5:27 PM -03:00

--- 1/mario.c
+++ 1/mario.c
@@ -2,18 +2,12 @@
 
 int main(void)
 {
-    // int i = 0;
-    // while(i <=4)
+
+    // for (int i = 0; i < 4; i++)
     // {
     //     printf("?");
-    //     i++;
     // }
     // printf("\n");
-    for (int i = 0; i < 4; i++)
-    {
-        printf("?");
-    }
-    printf("\n");
 
     int n = 3;
     for (int j = 0; j < n; j++)
