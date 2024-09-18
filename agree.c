adfaba10b464528eb436d4cdff165ab8e4ea00fd
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-18 17:43:34 +0000

    Wed, Sep 18, 2024, 2:43 PM -03:00

--- agree.c
+++ agree.c
@@ -5,13 +5,17 @@ int main(void)
 {
     char c = get_char("Voce aceita os termos? s ou n ");
 
-    if (c == 's' || 'S')
+    if (c == 's' || c == 'S')
     {
         printf("Aceito.\n");
     }
 
-    else if (c == 'n' || 'N')
+    else if (c == 'n' || c == 'N')
     {
         printf("Nao aceito.\n");
+    }
+    else (c )
+    {
+        printf("Resposta invalida.");
     }
 }
