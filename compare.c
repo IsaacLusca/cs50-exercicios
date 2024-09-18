bf190b4a00d22c7f92e75147af8eb777d2a5a0ec
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-18 17:10:01 +0000

    Wed, Sep 18, 2024, 2:10 PM -03:00

--- compare.c
+++ compare.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-    
+    int x = get_int("Valor de X");
+    int y = get_int("Valor de Y");
+
+    if (x > y)
+    {
+        printf("%i e maior que %i\n", x, y);
+    }
+    else if (x < y)
+    {
+        printf("%i e menor que %i\n", x, y);
+    }
+    else printf("%i e %i sao iguais", x, y);
 }
