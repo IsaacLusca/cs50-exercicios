247307e77c80e8426ae6c41d8e46858541aa8cd5
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-18 18:45:44 +0000

    Wed, Sep 18, 2024, 3:45 PM -03:00

---
+++ meow.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<cs50.h>
+
+void meow(void)
+{
+    printf("meow\n");
+}
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        meow();
+    }
+}
+
+
+
+
+
+
+
+// int main(void)
+// {
+//     for (int i = 0; i < 3; i++)
+//     {
+//         printf("Meow\n");
+//     }
+// }
+
+
+// int main(void)
+// {
+//     int i = 0;
+//     while( i < 3)
+//     {
+//         printf("meow\n");
+//         i++;
+//     }
+//     printf("%i\n", i);
+// }
