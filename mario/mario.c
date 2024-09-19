2084a8c54cebdc8d4e1df946575f07af5bb13b02
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-19 21:08:16 +0000

    Thu, Sep 19, 2024, 6:08 PM -03:00

--- mario/mario.c
+++ mario/mario.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<cs50.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -15,13 +15,14 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         // esse laço vai colocar o valor de entrada n no J e,
-        // enquanto j for maior que i, vai ser adicionado um campo vazio e o valor de J será subtraído
-        for (int j = n; j > i ; j--)
+        // enquanto j for maior que i, vai ser adicionado um campo vazio e j será subtraído
+        for (int j = n; j > i; j--)
         {
             printf(" ");
         }
         // nesse laço, k sempre será iniciado a zero e,
-        // enquanto k for menor ou igual que a variável i (que está no for inicial), será adicionado # até que k seja maior que i
+        // enquanto k for menor ou igual que a variável i (que está no for inicial),
+        // será adicionado # até que k seja maior que i
         for (int k = 0; k <= i; k++)
         {
             printf("#");
