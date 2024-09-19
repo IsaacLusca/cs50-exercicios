12372e944e57db733ad667d21fd71a127a210175
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-19 17:50:13 +0000

    Thu, Sep 19, 2024, 2:50 PM -03:00

--- 1/calculadora2.c
+++ 1/calculadora2.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
 #include<cs50.h>
 
-int div(int a, int b);
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%0.3f \n", div(x, y));
+    float z = (float)x / (float)y;
+    printf("%.2f\n", z);
 }
 
-int div (int a, int b)
-{
-    return a / b;
-}
