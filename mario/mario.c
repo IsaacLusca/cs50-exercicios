b6e207759a692374b3f4523e62526c25142cfa41
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-19 21:21:01 +0000

    Thu, Sep 19, 2024, 6:21 PM -03:00

--- mario/mario.c
+++ mario/mario.c
@@ -1,15 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int n;
+    // enquanto n for menor que 1, do será executado indeterminadamente
+    do
+    {
+        n = get_int("Tamanho: ");
+    }
+    while (n < 1);
 
-    // do {
-    //     n = get_int("Tamanho: ");
-    // } while (n < 1);
-
-    for (int i = 0; i < 5; i++) {
-        printf("%*s", 5 - i, ""); // Imprime espaços
-        printf("%.*s\n", i + 1, "#############################"); // Imprime hashes
+    // primeiro laço para percorrer toda estrutura e colocar a quebra de linha
+    for (int i = 0; i < n; i++)
+    {
+        // esse laço vai colocar o valor de entrada n no J e,
+        // enquanto j for maior que i, vai ser adicionado um campo vazio e j subtraído
+        for (int j = n; j > i; j--)
+        {
+            printf(" ");
+        }
+        // nesse laço, k sempre será iniciado a zero e,
+        // enquanto k for menor ou igual que a variável i (que está no for inicial),
+        // será adicionado # até que k seja maior que i
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
