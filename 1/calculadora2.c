44101e8008ca779ea222317961c1985fc1e761f3
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-19 17:38:37 +0000

    Thu, Sep 19, 2024, 2:38 PM -03:00

--- 1/calculadora2.c
+++ 1/calculadora2.c
@@ -5,7 +5,10 @@ int add(int a, int b);
 
 int main(void)
 {
-    int x = get_int
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+
+    printf("%i \n",add(x, y));
 }
 
 int add (int a, int b)
