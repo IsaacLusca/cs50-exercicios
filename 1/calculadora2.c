e907b855c08b357683cc1cc3d01c6217b70eab69
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-19 17:43:51 +0000

    Thu, Sep 19, 2024, 2:43 PM -03:00

--- 1/calculadora2.c
+++ 1/calculadora2.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 #include<cs50.h>
 
+int div(int a, int b);
 int main(void)
 {
+    float x = get_int("x: ");
+    float y = get_int("y: ");
 
+    printf("%f \n", div(x, y));
 }
 
+int div (float a, float b)
+{
+    return a / b;
+}
