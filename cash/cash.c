a3a2a85071d9b271500f16518ec05fc38fa53528
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-20 18:41:12 +0000

    Fri, Sep 20, 2024, 3:41 PM -03:00

--- cash/cash.c
+++ cash/cash.c
@@ -51,38 +51,4 @@ int main(void)
     printf("%i\n", moeda);
 }
 
-//O que devo fazer?
-// solicitar o troco
-
-
-
-
-
-
-
-
-
-    // if (valor < 5)
-    // {
-    //     int n = valor / A;
-    //     valor = A * n;
-    //     printf("%i\n", n);
-    // }
-
-    // if (valor >= 5 && valor < 10)
-    // {
-    //     int n = valor / A;
-    //     int z = valor / B;
-    //     valor = (B * z) + (A * n);
-    //     printf("%i\n", z + n);
-    // }
-
-    // if (valor >= 10 && valor < 25)
-    // {
-    //     int n = valor / A;
-    //     int z = valor / B;
-    //     int y = valor / C;
-    //     valor = (C * y) + (A * n) + (B * z);
-    //     printf("%i\n", y + n + z);
-    // }
 
