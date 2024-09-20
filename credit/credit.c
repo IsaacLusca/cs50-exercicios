ae34f7eba715226b11937aef0ea898c82b9f9908
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-20 19:44:17 +0000

    Fri, Sep 20, 2024, 4:44 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,27 +4,29 @@
 int main(void)
 {
 
-// long int cartao;
-// int resto = 0;
-// int sumResto = 0;
-// do
-// {
-//     cartao = get_long("Number: ");
-// }
-// while (cartao < 1);
+long int cartao;
+int resto = 0;
+int sumResto = 0;
+do
+{
+    cartao = get_long("Number: ");
+}
+while (cartao < 1);
 
-// while (cartao <= 10)
-// {
-//     resto = (cartao / 10) % 10;
-//     sumResto = resto + resto;
-// }
+while (divisao <= 10)
+{
+    long int divisao = (cartao / 10);
+    int resto = divisao % 10;
 
-long int divisao = (4003600000000014 / 10);
-int resto = divisao % 10;
-printf("%li\n%i\n", divisao, resto);
+    printf("%li\n%i\n", divisao, resto);
 }
 
-1 + 
+
+}
+
+
+
+
 // Entrada do usuário
 // calcular a soma da verificação.
 // Verificar comprimento do cartão para ver se condiz.
