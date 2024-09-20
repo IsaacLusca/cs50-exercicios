e46376f50bd29b7c890e1c45336d583fc989d8fb
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-20 03:24:52 +0000

    Fri, Sep 20, 2024, 12:24 AM -03:00

--- world/hello.c
+++ world/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("hello, world\n");
+int main() {
+    int a;
+    int b;
+    scanf("A: %i\n", &a);
+    scanf("B: %i\n", &b);
+
+    int x = a + b;
+
+    printf("X= %i", x);
+
+    return 0;
 }
