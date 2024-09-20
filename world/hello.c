33d8b579207b90f10d053cf62ef59645deb368ec
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-20 03:29:08 +0000

    Fri, Sep 20, 2024, 12:29 AM -03:00

--- world/hello.c
+++ world/hello.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    int b;
-    scanf("A: %i\n", &a);
-    scanf("B: %i\n", &b);
+    int A, B;
+    scanf("%d ,%d", &A, &B);  
 
-    int x = a + b;
+    int X = A + B;
 
-    printf("X= %i", x);
+    printf("X = %d\n", x);
 
     return 0;
 }
