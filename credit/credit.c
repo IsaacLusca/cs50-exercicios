9221710c64d2aff126a7327a95c9670622986b01
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-21 04:46:48 +0000

    Sat, Sep 21, 2024, 1:46 AM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,96 +4,98 @@
 int main(void)
 {
 
-long cartao;
-int sumResto = 0;
-int sumResto2 = 0;
-int tamanho = 0;
-// int primDigito;
+    long cartao;
+    int sumResto = 0;
+    int sumResto2 = 0;
+    int tamanho = 0;
+    // int primDigito;
 
-do
-{
-    cartao = get_long("Number: ");
-}
-while (cartao < 1);
-
-// Cartão temporário para o while
-long tempCartao = cartao;
-
-// função que soma e multiplica por 2os restos dos penúltimos dígitos
-while (tempCartao > 0)
-{
-    int resto = (tempCartao / 10) % 10;
-    int multResto = resto * 2;
-    if (multResto > 9)
+    do
     {
-        multResto = (multResto % 10) + 1;
+        cartao = get_long("Number: ");
     }
-    sumResto += multResto;
-    tempCartao /= 100;
-}
+    while (cartao < 1);
 
-long tempCartao2 = cartao;
+    // Cartão temporário para o while
+    long tempCartao = cartao;
 
-// função que soma os restos dos ultimos dígitos
-while (tempCartao2 > 0)
-{
-    int resto = tempCartao2 % 10;
-    sumResto2 += resto;
-    tempCartao2 /= 100;
-}
-
-int sum = sumResto + sumResto2;
-
-printf("%i\n", sum);
-
-  // Calcular o comprimento
-long tempCartao3 = cartao;
-while (tempCartao3 > 0)
-{
-    tempCartao3 /= 10;
-    tamanho++;
-}
-
-// Obter os dois primeiros dígitos
-long primeirosDigitos = cartao;
-while (primeirosDigitos >= 100)
-{
-    primeirosDigitos /= 10;
-}
-
-printf("%i\n", tamanho);
-printf("%li\n", primeirosDigitos);
-
-if ((sum % 10 == 0 || tamanho > 12 || tamanho < 17))
-{
-    if (primeirosDigitos == 37)
+    // função que soma e multiplica por 2os restos dos penúltimos dígitos
+    while (tempCartao > 0)
     {
-        printf("AMEX\n");
+        int resto = (tempCartao / 10) % 10;
+        int multResto = resto * 2;
+        if (multResto > 9)
+        {
+            multResto = (multResto % 10) + 1;
+        }
+        sumResto += multResto;
+        tempCartao /= 100;
     }
-    else if (primeirosDigitos == 22||
-        primeirosDigitos == 55||
-        primeirosDigitos == 51||
-        primeirosDigitos == 52)
+
+    long tempCartao2 = cartao;
+
+    // função que soma os restos dos ultimos dígitos
+    while (tempCartao2 > 0)
     {
-        printf("MASTERCARD\n");
+        int resto = tempCartao2 % 10;
+        sumResto2 += resto;
+        tempCartao2 /= 100;
     }
-    else if (primeirosDigitos == 41||
-        primeirosDigitos == 40||
-        primeirosDigitos == 42||
-        primeirosDigitos == 49)
+
+    int sum = sumResto + sumResto2;
+
+
+    // Calcular o comprimento
+    long tempCartao3 = cartao;
+    while (tempCartao3 > 0)
     {
-        printf("VISA\n");
+        tempCartao3 /= 10;
+        tamanho++;
+    }
+
+    // Obter os dois primeiros dígitos
+    long primeirosDigitos = cartao;
+    while (primeirosDigitos >= 100)
+    {
+        primeirosDigitos /= 10;
+    }
+
+
+
+    // printf("SUM: %i\n", sum);
+    // printf("TAMANHO: %i\n", tamanho);
+    // printf("PRIMEIROS DIGITOS: %li\n", primeirosDigitos);
+    // printf("VALOR DO RESTO %i", teste);
+
+    if ((sum % 10 == 0 && tamanho > 12 && tamanho < 17))
+    {
+        if (primeirosDigitos == 37)
+        {
+            printf("AMEX\n");
+        }
+        else if (primeirosDigitos == 22||
+            primeirosDigitos == 55||
+            primeirosDigitos == 51||
+            primeirosDigitos == 52)
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (primeirosDigitos == 41||
+            primeirosDigitos == 40||
+            primeirosDigitos == 42||
+            primeirosDigitos == 49)
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     else
     {
         printf("INVALID\n");
     }
-}
-else
-{
-    printf("INVALID\n");
-}
-
 }
 
 
