8112f23992ddbe3d838f2182838352b3e5153271
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-22 15:07:39 +0000

    Sun, Sep 22, 2024, 12:07 PM -03:00

--- 2/array.c
+++ 2/array.c
@@ -1,25 +1,43 @@
+#include <cs50.h>
 #include <stdio.h>
+
+int hashTamanho(int tamanho);
 int main(void)
 {
-    int bloco = 4;
+    int t = get_int("Tamanho: ");
+    hashTamanho(t);
+}
 
-    for (int i = 0; i < bloco; i++)
+int hashTamanho(int tamanho)
+{
+    for(int i = 0; i < tamanho; i++)
     {
-        for (int j = bloco; j - 1 > i; j--)
-        {
-            printf(" ");
-        }
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-        }
-        printf("  ");
-
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
+        printf("#\n");
     }
 }
+
+
+
+
+
+    // int bloco = 4;
+
+    // for (int i = 0; i < bloco; i++)
+    // {
+    //     for (int j = bloco; j - 1 > i; j--)
+    //     {
+    //         printf(" ");
+    //     }
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         printf("#");
+    //     }
+    //     printf("  ");
+
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         printf("#");
+    //     }
+
+    //     printf("\n");
+    // }
