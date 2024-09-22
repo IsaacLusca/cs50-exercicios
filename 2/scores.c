d2498038f437ad38d5bad87ab8540573a3b6d62e
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-22 16:36:03 +0000

    Sun, Sep 22, 2024, 1:36 PM -03:00

--- 2/scores.c
+++ 2/scores.c
@@ -1,24 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int N = 3;
+
 int main(void)
 {
-    // int scores[3];
-    // scores[0] = get_int("Nota: ");
-    // scores[1] = get_int("Nota: ");
-    // scores[2] = get_int("Nota: ");
-
-    const int N = 3;
     int scores[N];
-
-    // n = get_int("Qnt: ");
 
     for (int i = 0; i < N; i++)
     {
         scores[i] = get_int("nota: ");
     }
 
-    printf("Media: %.2f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
+}
+
+float media(int tamanho, int array [])
+{
+    int sum = 0;
+    for ()
 }
 
     // int notas = 0;
