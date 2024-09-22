177e8490f552eadf1efd9cd784f7ebdc34736d89
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-22 15:38:53 +0000

    Sun, Sep 22, 2024, 12:38 PM -03:00

--- 2/scores.c
+++ 2/scores.c
@@ -3,10 +3,21 @@
 
 int main(void)
 {
-    int notas;
+    int notas = 0;
     int qnt;
     do
     {
-        
+        qnt = get_int("Quantidade: \n");
+
+        for (int i = 1; i <= qnt; i++)
+        {
+            notas = get_int("nota %i: \n", i);
+            notas += notas;
+        }
     }
+    while (qnt < 1);
+
+    int media = notas / qnt;
+
+    printf("%i",media);
 }
