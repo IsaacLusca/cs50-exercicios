c60f093f0a419543664fddeef26f9b1e3836dddf
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-22 16:03:59 +0000

    Sun, Sep 22, 2024, 1:03 PM -03:00

--- 2/scores.c
+++ 2/scores.c
@@ -4,9 +4,9 @@
 int main(void)
 {
     int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    scores[0] = get_int("Nota: ");
+    scores[1] = get_int("Nota: ");
+    scores[2] = get_int("Nota: ");
 
     printf("Media: %.2f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
