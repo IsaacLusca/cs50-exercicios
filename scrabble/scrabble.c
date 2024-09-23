e3eb000d0d8f75564c5a9b2248e1dc7be79ae7eb
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-23 19:56:10 +0000

    Mon, Sep 23, 2024, 4:56 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,12 +5,18 @@
 
 int main(int argc, string argv[])
 {
+    int sum1 = 0;
+
     string palavra1 = get_string("Player 1: ");
     string palavra2 = get_string("Player 2: ");
 
     for (int i = 0, n = strlen(palavra1); i < n; i++)
     {
-        // if (palavra1[i] == '')
+        if (palavra1[i] == 'A'||'E'||'L'||'N'||'O'||'R'||'S'||'T'||'U')
+        {
+            sum1++;
+        }
+        else if (palavra1[i] == 'A'||'E'||'L'||'N'||'O'||'R'||'S'||'T'||'U')
 
         // printf("%c", toupper(palavra1[i]));
     }
