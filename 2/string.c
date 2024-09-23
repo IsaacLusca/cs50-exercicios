654c521d20df3844579f9c15c25683290c91072b
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-23 15:12:59 +0000

    Mon, Sep 23, 2024, 12:12 PM -03:00

---
+++ 2/string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string s = get_string("Entrada: ");
+    printf("Saída: ");
+
+    for(int i = 0; i < strlen(s); i++)
+    
+}
