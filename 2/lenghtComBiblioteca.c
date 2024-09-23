1fde2c6559352a8e83c378e43e5843070be4b6c1
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-23 15:05:25 +0000

    Mon, Sep 23, 2024, 12:05 PM -03:00

--- 2/lenghtComBiblioteca.c
+++ 2/lenghtComBiblioteca.c
@@ -1,1 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+    string nome = get_string("Nome: ");
+    int lenght = strlen(nome);
+
+    printf("O nome %s tem %i caracteres\n", nome, lenght);
+
+}
 
