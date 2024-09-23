46c5a3c08d2c26b349bb68534ef201eb7065779d
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-23 20:37:13 +0000

    Mon, Sep 23, 2024, 5:37 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -11,7 +11,7 @@ int main(int argc, string argv[])
     string palavra1 = get_string("Player 1: ");
     string palavra2 = get_string("Player 2: ");
 
-    
+
 
     if(sum1 == sum2)
     {
@@ -32,5 +32,41 @@ int main(int argc, string argv[])
 
 int calcular_pontuacao(string palavra)
 {
+    for (int i = 0, n = strlen(palavra); i < n; i++)
+    {
+        palavra[i] = toupper(palavra[i]);
 
+        if (palavra[i] == 'A' || palavra[i] == 'E' || palavra[i] == 'I' || palavra[i] == 'L' ||
+            palavra[i] == 'N' || palavra[i] == 'O' || palavra[i] == 'R' || palavra[i] == 'S' ||
+            palavra[i] == 'T' || palavra[i] == 'U')
+        {
+            sum1 += 1;
+        }
+        else if (palavra[i] == 'D' || palavra[i] == 'G')
+        {
+            sum1 += 2;
+        }
+        else if (palavra[i] == 'B' || palavra[i] == 'C' || palavra[i] == 'M' || palavra[i] == 'P')
+        {
+            sum1 += 3;
+        }
+        else if (palavra[i] == 'F' || palavra[i] == 'H' || palavra[i] == 'V' || palavra[i] == 'W' || palavra[i] == 'Y')
+        {
+            sum1 += 4;
+        }
+        else if (palavra[i] == 'K')
+        {
+            sum1 += 5;
+        }
+        else if (palavra[i] == 'J' || palavra[i] == 'X')
+        {
+            sum1 += 8;
+        }
+        else if (palavra[i] == 'Q' || palavra[i] == 'Z')
+        {
+            sum1 += 10;
+        }
+
+        // printf("%c", toupper(palavra[i]));
+    }
 }
