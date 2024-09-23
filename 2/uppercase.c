8a4742a661cd7000f56f18f5ea38505f24c693cf
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-23 16:47:28 +0000

    Mon, Sep 23, 2024, 1:47 PM -03:00

--- 2/uppercase.c
+++ 2/uppercase.c
@@ -10,9 +10,7 @@ int main(void)
     for(int i = 0, n = strlen(s); i < n; i++)
     {
         // Se o lowercase estiver entre a e z
-        if(s[i] => 'a' && s[i] <= 'z')
-        {
-            printf("%c", s[i]toupper)
-        }
+        é
     }
+    printf("\n");
 }
