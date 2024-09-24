2a9b8d4be1351d54809d79b709e506225569d951
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 13:58:10 +0000

    Tue, Sep 24, 2024, 10:58 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,23 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
-#include <stdio.h.h>
+#include <stdio.h>
 
 int main(void)
 {
-    
+    int x = 0;
+    string texto[x] = get_string("Text: ");
+
+    if (texto[x] == ' ')
+    {
+        total = t - 1;
+    }
+    else
+    {
+        int t = strlen(texto);
+    }
+
+    printf("%i\n", t);
 }
 
 //
