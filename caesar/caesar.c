cdeff1e0a177c596629b4ace771fc7a520eaa93a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 20:05:03 +0000

    Tue, Sep 24, 2024, 5:05 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,17 +9,11 @@ int main(int argc, string argv[])
     string texto = get_string("Text: ");
     bool dentro_palavra = false;
 
-    int num = 0;
-
     for (int i = 0, n = strlen(texto); i < n; i++)
     {
         // isalpha verifica se é uma letra do alfabeto
         if(!isalpha(texto[i]))
         {
-            if (num = texto[i] > 25)
-            {
-                num = 0;
-            }
             printf("%c", texto[i]);
         }
         else
