de80d20458404b138a0d5675a3c325d401650f2c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 13:16:34 +0000

    Tue, Sep 24, 2024, 10:16 AM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int calcular_pontuacao(string palavra);
 int main(int argc, string argv[])
@@ -10,31 +10,32 @@ int main(int argc, string argv[])
     string palavra1 = get_string("Player 1: ");
     string palavra2 = get_string("Player 2: ");
 
-
+    // Chamando a função de pontuação
     int sum1 = calcular_pontuacao(palavra1);
     int sum2 = calcular_pontuacao(palavra2);
 
-
-    if(sum1 == sum2)
+    // Comparando valores
+    if (sum1 == sum2)
     {
         printf("Tie!\n");
     }
-    if(sum1 > sum2)
+    if (sum1 > sum2)
     {
         printf("Player 1 wins!\n");
     }
-    if(sum1 < sum2)
+    if (sum1 < sum2)
     {
         printf("Player 2 wins!\n");
     }
 
-
     return 0;
 }
 
+// Função para calcular pontuação
 int calcular_pontuacao(string palavra)
 {
-    int points[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    int points[26] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
+                      1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     int sum = 0;
     // Comparar e atribuir os valoress
     for (int i = 0, n = strlen(palavra); i < n; i++)
