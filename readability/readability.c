5866e37deaf5f68ae0d7649ec76f836eb326b41e
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 15:27:35 +0000

    Tue, Sep 24, 2024, 12:27 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,38 +3,58 @@
 #include <string.h>
 #include <stdio.h>
 
-int contador_letras(string txt);
+// int contador_letras(string txt);
 int main(void)
 {
+    int let = 0;
     int pal = 0;
     int sent = 0;
 
     string texto = get_string("Text: ");
 
-    int letras = contador_letras(texto);
-    printf("letras: %i\n",letras);
-}
-
-int contador_letras(string txt)
-{
-    int let = 0;
-    for (int i = 0, n = strlen(txt); i < n; i++)
+    // int letras = contador_letras(texto);
+    for (int i = 0, n = strlen(texto); i < n; i++)
     {
-        if (txt[i] == ' '||
-            txt[i] == '. '||
-            txt[i] == ', '||
-            txt[i] == '? '||
-            txt[i] == '! '||
-            txt[i] == '/ '||
-            txt[i] == '\''
+    if (texto[i] == ' '||
+        texto[i] == '.'||
+        texto[i] == ','||
+        texto[i] == '?'||
+        texto[i] == '!'||
+        texto[i] == '/'||
+        texto[i] == '\''
         )
         {
             let--;
+            pal++;
         }
         let++;
     }
-    return let;
+
+    printf("letras: %i\nPalavras: %i\n",let, pal);
 }
+
+// int contador_letras(string txt)
+// {
+//     int let = 0;
+//     int pal = 0;
+//     for (int i = 0, n = strlen(txt); i < n; i++)
+//     {
+//         if (txt[i] == ' '||
+//             txt[i] == '.'||
+//             txt[i] == ','||
+//             txt[i] == '?'||
+//             txt[i] == '!'||
+//             txt[i] == '/'||
+//             txt[i] == '\''
+//         )
+//         {
+//             let--;
+//             pal++;
+//         }
+//         let++;
+//     }
+//     return let, pal;
+// }
 
 
 // calcular qtd de letra
