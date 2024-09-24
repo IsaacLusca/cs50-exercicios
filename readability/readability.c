4ef5b56dbe069aea4c6f0d34f1bed0a254e21a96
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 15:00:02 +0000

    Tue, Sep 24, 2024, 12:00 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,7 +5,17 @@
 
 int main(void)
 {
+    int let = 0;
+    int pal = 0;
+    int sent = 0;
 
+    string texto = get_string("Text: ");
+
+    for (int i = let; i < strlen(texto); i++)
+    {
+        let = strlen(texto);
+    }
+    printf("letras: %i\n",let);
 }
 
 // calcular qtd de letra
