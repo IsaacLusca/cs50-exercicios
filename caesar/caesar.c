cdea04635b8aea90d40f912b655065abf2909147
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 19:32:42 +0000

    Tue, Sep 24, 2024, 4:32 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,15 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
     int chave = get_int("Chave: ");
     string texto = get_string("Text: ");
+    bool dentro_palavra = false;
 
     for (int i = 0, n = strlen(texto); i < n; i++)
     {
-        printf("%c", (texto[i] + chave) % 26);
+        if (isalpha(texto[i]))
+        {
+            if(!dentro_palavra)
+            {
+                dentro_palavra = true;
+                printf("%c", (texto[i] + chave) % 26);
+            }
+            else
+            {
+                printf("%c", texto[i]);
+            }
+        }
     }
     printf("\n");
 }
