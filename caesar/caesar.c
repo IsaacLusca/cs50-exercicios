3c54c24d86563ef96cd575575c92629f53ee0a2a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 19:18:14 +0000

    Tue, Sep 24, 2024, 4:18 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    
+    int chave = get_int("Chave: ");
+    string texto = get_string("Text: ");
+
+    for (int i = 0, n = strlen(texto); i < n; i++)
+    {
+        printf("%c", texto[i] + n);
+    }
 }
