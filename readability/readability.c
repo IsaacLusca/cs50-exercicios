8d8d47e81062fa8307d89266dcc107c55b004cde
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 14:54:20 +0000

    Tue, Sep 24, 2024, 11:54 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,57 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
-int string_comprimento(string c);
 int main(void)
 {
-    string nome = get_string("Nome: ");
-
-    int lenght = string_comprimento(nome);
-
-    printf("O nome %s tem %i caracteres\n", nome, lenght);
 
 }
-
-int string_comprimento(string c)
-{
-    int n = 0;
-    for (int i = 0; c[i] != '\0'; i++)
-    {
-        if (c[i] == ' '||
-            c[i] == ','||
-            c[i] == '.'||
-            c[i] == ':'
-            )
-        {
-            n--;
-        }
-        n++;
-    }
-    return n;
-}
-
-// Palavras longas tem um "maior nível"
-// Frases longas tem um "Maior
-
-    // Ignora os espaços vazios
-    // for (int i = 0; i < strlen(texto); i++)
-    // {
-    //     if (texto [i] != ' ')
-    //     {
-    //         t++;
-    //     }
-    // }
-
-    // for (int i = 0; i < strlen(texto); i++)
-    // {
-    //     if (texto[i] == ' ')
-    //     {
-    //         palavra[n] = t;
-    //         n++;
-    //         t = 0; // Reinicia o contador de comprimento da palavra
-    //     }
-    //     else
-    //     {
-    //         t++; // Incrementa o comprimento da palavra atual
-    //     }
-    // }
