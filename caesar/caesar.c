91879797cbadf3a58f966e9613fabaf61b00d687
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 20:44:00 +0000

    Tue, Sep 24, 2024, 5:44 PM -03:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -14,26 +14,19 @@ int main(int argc, string argv[])
         // isalpha verifica se é uma letra do alfabeto
         if (isalpha(texto[i]))
         {
+            // Para letras maiusculas
             if (isupper(texto[i]))
             {
                 printf("%c", 'A' + (texto[i] - 'A' + 13) % 26);
             }
+            // Para minusculas
             if (islower(texto[i]))
             {
                 printf("%c", 'a' + (texto[i] - 'a' + 13) % 26);
             }
         }
         else (printf("%c", texto[i]));
-
-
     }
+
     printf("\n");
-
-    printf("a: %i\n", 'a');
-    printf("b: %i\n", 'b');
-    printf("A: %i\n", 'A');
-    printf("B: %i\n", 'B');
-    printf("A+1: %c\n", 68 % 26);
-    printf("a+1: %c\n", 111 % 26);
-
 }
