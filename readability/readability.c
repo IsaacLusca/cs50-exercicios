7b34fa1ebb23ef706280fcc0f0380e85580e3289
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-24 14:48:38 +0000

    Tue, Sep 24, 2024, 11:48 AM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,12 +8,6 @@ int main(void)
 {
     string nome = get_string("Nome: ");
 
-    // Enquanto nome for diferente de \0, faça
-    // while (nome[n] != '\0')
-    // {
-    //     n++;
-    // }
-
     int lenght = string_comprimento(nome);
 
     printf("O nome %s tem %i caracteres\n", nome, lenght);
@@ -25,7 +19,11 @@ int string_comprimento(string c)
     int n = 0;
     for (int i = 0; c[i] != '\0'; i++)
     {
-        if (c[i] == ' ')
+        if (c[i] == ' '||
+            c[i] == ','||
+            c[i] == '.'||
+            c[i] == ':'
+            )
         {
             n--;
         }
