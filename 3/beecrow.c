2f08b8db90506e326293d912310d50fe021f04e6
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 14:34:25 +0000

    Thu, Sep 26, 2024, 11:34 AM -03:00

---
+++ 3/beecrow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a;
+    int b;
+    int s;
+    scanf("a: %i\n", a);
+    scanf("b: %i\n", b);
+    scanf("s: %i\n", s);
+
+    int maiorAB = (a + b + a*b*s*(a-b))/2;
+
+    printf("maior: \n", maiorAB);
+
+    return 0;
+}
