d2ddcd432159884681b50c337de4b45994b6eb2f
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 17:49:51 +0000

    Thu, Sep 26, 2024, 2:49 PM -03:00

--- 3/phonebook.c
+++ 3/phonebook.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    string nome[] = {"Carter", "David", "John"};
+    string numeros[] = {"438-328-1000", "485-973-1023", "348-129-5748"};
+}pessoas
+
 int main(void)
 {
-    string name[] = {"Carter", "David", "John"};
-    string numbers[] = {"438-328-1000", "485-973-1023", "348-129-5748"};
 
     string s = get_string("Nome: ");
 
     for (int i = 0; i < 3; i++)
     {
-        if(strcmp(name[i], s))
+        if(strcmp(nome[i], s))
         {
-            printf("Número: %s\n", numbers[i]);
+            printf("Número: %s\n", numeros[i]);
             return 0;
         }
     }
