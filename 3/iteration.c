0e52a4c2bcbf804e510b15517f00048b084f7b9d
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 23:53:57 +0000

    Thu, Sep 26, 2024, 8:53 PM -03:00

---
+++ 3/iteration.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+int main(void)
+{
+    int altura = get_int("Altura: ");
+
+    draw(altura);
+}
+
+void draw(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
