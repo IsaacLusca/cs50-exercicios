a4b2649e49698c599e69c39658cb8aa7db9c06d4
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 15:25:23 +0000

    Thu, Sep 26, 2024, 12:25 PM -03:00

--- 3/beecrow.c
+++ 3/beecrow.c
@@ -3,7 +3,7 @@
 
 int main() {
     int matriz[26] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
-    int x = 15;
+    int x = 100;
     for (int i = 0; i < 26; i++)
     {
         if ( matriz[i] == x)
@@ -11,4 +11,5 @@ int main() {
             printf("%i - O valor está na matriz!\n", i+1);
         }
     }
+    return 2;
 }
