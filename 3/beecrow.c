1a7f35193005cd8e58f598fecd53ad5531ca44f7
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 15:11:16 +0000

    Thu, Sep 26, 2024, 12:11 PM -03:00

--- 3/beecrow.c
+++ 3/beecrow.c
@@ -3,23 +3,5 @@
 
 int main() {
 
-    int a;
-    int b;
-    int s;
-    scanf("%i", &a);
-    scanf("%i", &b);
-    scanf("%i", &s);
-
-    int maiorAB = (a + b + abs(a-b))/2;
-
-    if (maiorAB > s)
-    {
-        printf("%i eh o maior\n", maiorAB);
-    }
-    else
-    {
-        printf("%i eh o maior\n", s);
-    }
-
-    return 0;
+    matriz[26] = {1, 2, 3, 4}
 }
