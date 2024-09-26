c834b8e84d90c00d71c7f94fe0b618ba88486ad9
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 17:02:52 +0000

    Thu, Sep 26, 2024, 2:02 PM -03:00

--- 3/search.c
+++ 3/search.c
@@ -2,5 +2,16 @@
 #include <cs50.h>
 
 int main() {
-    int num
+    int num[] = {20, 500, 10, 5, 100, 1, 50};
+
+    int n = get_int("Número: ");
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (n == num[i])
+        {
+            i++;
+        }
+    }
+
 }
