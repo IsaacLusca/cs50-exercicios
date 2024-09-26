a0476f1366a93a2e9c516f0ce861d28f57953b1a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 17:45:25 +0000

    Thu, Sep 26, 2024, 2:45 PM -03:00

--- 3/phonebook.c
+++ 3/phonebook.c
@@ -4,9 +4,18 @@
 
 int main(void)
 {
-    string name[] = {"Carter", "David", "John"};
-    string numbers[] = {"438-328-1000", "485-973-1023", "348-129-5748"};
+    string name[3][3] = {"Carter", "David", "John", "438-328-1000", "485-973-1023", "348-129-5748"};
 
+    string s = get_string("Nome: ");
 
-    
+    for (int i = 0; i < 3; i++)
+    {
+        if(strcmp(name[i], s))
+        {
+            printf("Número: %s\n", name[][i]);
+            return 0;
+        }
+    }
+    printf("Não encontrado");
+    return 1;
 }
