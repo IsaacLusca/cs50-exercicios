e980a3f036c16bbdcae8dd5b1262cde392240fb6
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-26 17:12:25 +0000

    Thu, Sep 26, 2024, 2:12 PM -03:00

--- 3/search.c
+++ 3/search.c
@@ -2,20 +2,22 @@
 #include <cs50.h>
 
 int main() {
-    int num[] = {20, 500, 10, 5, 100, 1, 50};
-
-    int n = get_int("Número: ");
-
-    // Busca linear
-    for (int i = 0; i < 7; i++)
-    {
-        if (n == num[i])
-        {
-            printf("Encontrado\n");
-            return 0;
-        }
-
-    }
-    printf("Não encontrado\n");
-    return 1;
+    
 }
+
+    // // Busca linear
+    // int num[] = {20, 500, 10, 5, 100, 1, 50};
+
+    // int n = get_int("Número: ");
+
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     if (n == num[i])
+    //     {
+    //         printf("Encontrado\n");
+    //         return 0;
+    //     }
+
+    // }
+    // printf("Não encontrado\n");
+    // return 1;
