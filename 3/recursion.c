6613416de978c595a8ac7802400a42fa6578a724
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-27 00:02:32 +0000

    Thu, Sep 26, 2024, 9:02 PM -03:00

--- 3/recursion.c
+++ 3/recursion.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void draw(int n);
+void draw(int n)
 int main(void)
 {
     int altura = get_int("Altura: ");
@@ -11,12 +11,11 @@ int main(void)
 
 void draw(int n)
 {
+    draw(n - 1);
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        printf("#");
     }
+    printf("#");
 }
