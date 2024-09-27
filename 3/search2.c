477a4f4e362e7acd33102de0bd77167a8ef87cb9
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-27 15:09:03 +0000

    Fri, Sep 27, 2024, 12:09 PM -03:00

--- 3/search2.c
+++ 3/search2.c
@@ -9,8 +9,38 @@ typedef struct
 
 int main(void)
 {
-    candidatos nome;
-    candidatos voto;
+    int num = 3;
+    candidatos presidente[num];
 
-    nome
+    presidente[0].nome = "bolsonaro";
+    presidente[0].voto = 100;
+
+    presidente[1].nome = "Lula";
+    presidente[1].voto = 100;
+
+    presidente[2].nome = "Aecio";
+    presidente[2].voto = 20;
+
+    int maior_votos = 0;
+    for (int i = 0; i < num; i++)
+    {
+        if (presidente[i].voto > maior_votos)
+        {
+            maior_votos = presidente[i].voto;
+        }
+        else if (presidente[i].voto == maior_votos)
+        {
+            printf("Houve empate, vamos reiniciar!\n");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        if (presidente[i].voto == maior_votos)
+        {
+            printf("%s\n", presidente[i].nome);
+        }
+    }
+    printf("%i\n", maior_votos);
 }
