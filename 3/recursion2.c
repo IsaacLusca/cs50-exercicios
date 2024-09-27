098af6da08bec1caaad2e9ed7b4cd0c58857da1f
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-27 15:33:24 +0000

    Fri, Sep 27, 2024, 12:33 PM -03:00

--- 3/recursion2.c
+++ 3/recursion2.c
@@ -3,13 +3,30 @@
 
 int main(void)
 {
-    int resultado = 0;
-    int n = get_int("Fatorial de: ");
-
-    for (int i = n; i > 0 ; i--)
-    {
-        i = i * (i - 1);
-    }
-
-    printf("resultado: %i\n", n);
+    
 }
+
+
+
+// CALCULO FATORIAL SEM RECURSÃO
+
+// int main(void)
+// {
+//     int resultado = 1;
+//     int n = get_int("Fatorial de: ");
+
+//     for (int i = n; i > 0 ; i--)
+//     {
+//         resultado *= i;
+//         if (i != 1)
+//         {
+//             printf("%i * ", i);
+//         }
+//         else
+//         {
+//             printf("%i", i);
+//         }
+//     }
+
+//     printf(" = %i\n", resultado);
+// }
