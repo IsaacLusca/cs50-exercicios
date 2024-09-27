fd3da14658f08b8a23956d4769bf5c64976e58c3
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-27 18:40:33 +0000

    Fri, Sep 27, 2024, 3:40 PM -03:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -84,5 +84,10 @@ bool vote(string name)
 void print_winner(void)
 {
     // imprimir o candidato ou candidatos mais votados.
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if ()
+    }
+
     return;
 }
