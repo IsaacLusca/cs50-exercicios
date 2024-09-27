89dd249efedf32cb45264efe04a649355469cf3b
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-27 16:54:39 +0000

    Fri, Sep 27, 2024, 1:54 PM -03:00

--- 3/exercice.c
+++ 3/exercice.c
@@ -1,22 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
+int collatz(int n);
 int main(void)
 {
+    int num = get_int("Número collatz: ");
 
+    printf("%i", collatz(num)/n);
 }
 
 int collatz(int n)
 {
     if (n == 1)
-    {
         return 0;
-    }
-    else if (n == 2)
-    {
-        return collatz ;
-    }
 
-    return n;
+    else if (n % 2 == 0)
+        return 1 + collatz(n/2);
+
+    else
+        return 1 + collatz(3*n + 1);
+
 }
