d1f3732df544b0f6f4ce1c098d9f415abb308b3b
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-29 16:51:10 +0000

    Sun, Sep 29, 2024, 1:51 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,11 +143,11 @@ void tabulate(void)
 {
     int contagem = 0;
 
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < preferences[i]; i++)
     {
-        for (int j = 0; j < candidate_count; i++)
+        for (int j = 0; j < preferences[i][j]; i++)
         {
-            
+            contagem = contagem + preferences[i][j];
         }
     }
 
