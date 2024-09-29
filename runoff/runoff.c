7b0c899994eff0901a4b4ff0ccfc401a9361ffaf
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-29 16:26:18 +0000

    Sun, Sep 29, 2024, 1:26 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -126,7 +126,14 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidate[i].name, name) == 0)
+        {
+            
+        }
+    }
+
     return false;
 }
 
