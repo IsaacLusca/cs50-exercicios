96507878b09ea10e6daf6dc5ae06e646bef4a4aa
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-30 17:11:33 +0000

    Mon, Sep 30, 2024, 2:11 PM -03:00

--- 4/copy.c
+++ 4/copy.c
@@ -5,6 +5,12 @@
 
 int main(void)
 {
-    string s = get_string("S: ")
+    string s = get_string("S: ");
+    // Está atribuindo e dizendo que o endereço de S é o mesmo que T
     string t = s;
+
+    t[0] = toupper(t[0]);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
 }
