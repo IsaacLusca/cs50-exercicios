ecafd099683ba1280fd982666b6329c4d356a73f
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-30 16:42:16 +0000

    Mon, Sep 30, 2024, 1:42 PM -03:00

--- 4/addresses.c
+++ 4/addresses.c
@@ -1,15 +1,16 @@
 // #include <cs50.h>
 #include <stdio.h>
-#include <strlen.h>
+#include <string.h>
 
 int main(void)
 {
     char *s = "HI!";
 
-    for (int i = 0 n = strlen(s); i < n; i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
         printf("%c", s[i]);
     }
+    printf("\n");
 
     // printf("%s\n", s);
 
