0ef67b15a0821d9f8cc674d3bcf9cd0f46f62476
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-30 17:16:54 +0000

    Mon, Sep 30, 2024, 2:16 PM -03:00

--- 4/copy.c
+++ 4/copy.c
@@ -2,15 +2,26 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    string s = get_string("S: ");
-    // Está atribuindo e dizendo que o endereço de S é o mesmo que T
-    string t = s;
+    char *s = get_string("S: ");
+    // Para atribuir o valor de S, não o endereço para t:
+    // malloc()
+    char *t = ;
 
     t[0] = toupper(t[0]);
 
     printf("%s\n", s);
     printf("%s\n", t);
 }
+
+    // string s = get_string("S: ");
+    // // Está atribuindo e dizendo que o endereço de S é o mesmo que T
+    // string t = s;
+
+    // t[0] = toupper(t[0]);
+
+    // printf("%s\n", s);
+    // printf("%s\n", t);
