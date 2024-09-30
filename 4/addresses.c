3730e2a47ca776b05c81cd11d0c9e322740891b8
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-09-30 15:59:10 +0000

    Mon, Sep 30, 2024, 12:59 PM -03:00

--- 4/addresses.c
+++ 4/addresses.c
@@ -5,6 +5,11 @@ int main(void)
 {
     string s = "HI!";
     printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+    printf("%p\n", &s[4]);
 }
 
 
