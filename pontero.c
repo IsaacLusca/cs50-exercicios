842c2dfde42ef701ebbdce45332e667c2c940e5f
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-01 19:11:44 +0000

    Tue, Oct 1, 2024, 4:11 PM -03:00

--- pontero.c
+++ pontero.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int x, int y);
 int main()
 {
-    int temp, a, b;
-    a = 1;
-    b = 2;
 
-    temp = a;
-    a = b;
-    b = temp;
-    printf("a: %i e b: %i\n", a, b);
+    int a = 1;
+    int b = 5;
+
+    printf("a: %i e b: %i\n", swap(a, b));
+
+}
+
+void swap(int x, int y)
+{
+    temp = x;
+    x = y;
+    y = temp;
 }
