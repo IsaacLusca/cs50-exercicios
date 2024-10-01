e20e2d09e528c02bb5c30af4f56c934bfea78a7b
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-01 19:31:47 +0000

    Tue, Oct 1, 2024, 4:31 PM -03:00

--- pontero.c
+++ pontero.c
@@ -5,11 +5,14 @@ void swap(int *x, int *y);
 int main()
 {
 
-    int *a = 1;
-    int *b = 5;
+    int *a;
+    int *b;
+
+    a = 0;
+    b = 0;
 
     // swap(&a, &b);
-    printf("a: %i e b: %i\n", *a, *b);
+    printf("a: %i e b: %i\n", a, b);
 }
 
 // void swap(int *x, int *y)
