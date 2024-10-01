ca9dbe8cfba77b867073c5bb89c0c43dddf0a084
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-01 16:15:22 +0000

    Tue, Oct 1, 2024, 1:15 PM -03:00

--- 4/cp.c
+++ 4/cp.c
@@ -3,3 +3,11 @@
 
 // Retorna dados brutos, 8 bits, sem valor negativo e apenas dados brutos
 typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+    // rb é leitura - read, binária, pegando apenas imagens, etcc
+    FILE *src = fopen(argv[1], "rb");
+    FILE *dst = fopen(argv[2], "wb");
+
+}
