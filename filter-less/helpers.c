3c76db083884f9906b30f88428abb62ac757e1b6
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-05 17:21:19 +0000

    Sat, Oct 5, 2024, 2:21 PM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,7 +5,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; i++)
+        for (int j = 0; j < width; j++)
         {
             image[i][j]
         }
