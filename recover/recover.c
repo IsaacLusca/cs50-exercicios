e43d9db56c78e3aea229175e85fae1cae0fdec37
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-07 16:41:30 +0000

    Mon, Oct 7, 2024, 1:41 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,11 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    if (argv[1])
+    FILE *f = fopen(card.raw, "r");
+    
 }
 
 // obrir o card
 // procurar na memoria algum inicio de arquivo jpeg
-// se encontrado, deve abrir uma nova jpege gravara blocos de 512 bytes
-// 
+// se encontrado, deve abrir uma nova jpege
+// gravara blocos de 512 bytes ate encontrar um novo arquivo jpeg
+//
 
