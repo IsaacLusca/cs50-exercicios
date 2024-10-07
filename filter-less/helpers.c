b141ba33bffed6ae1bfe7111a5da74baa537bfef
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-07 14:11:45 +0000

    Mon, Oct 7, 2024, 11:11 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,13 +1,16 @@
 #include "helpers.h"
 
 // Convert image to grayscale
+// rgbtBlue;
+// rgbtGreen;
+// rgbtRed;
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j]
+            image[i][j].rgbtBlue = 0;
         }
     }
     return;
