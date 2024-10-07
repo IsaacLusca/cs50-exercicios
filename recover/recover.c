842e83995675451e03694f896d517ce249e9b04c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-07 18:49:56 +0000

    Mon, Oct 7, 2024, 3:49 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -14,13 +14,12 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, sizeof(BYTE), HEADER_SIZE, f) == HEADER_SIZE)
     {
-        FILE *nova_img = fopen("recover.jpeg", "w");
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             {
-                while (fread(buffer, sizeof(BYTE), HEADER_SIZE, f) != 0)
-                {
-                    fwrite(&buffer, syzeof(BYTE), HEADER_SIZE, "recover.jpeg");
-                }
+                FILE *nova_img = fopen("recover.jpeg", "w");
+                fwrite(buffer, sizeof(BYTE), HEADER_SIZE, nova_img);
+
+                
             }
 
     }
