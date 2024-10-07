db4bd4f68ea2d29b90e88e5123d47ad98289637d
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-07 19:23:42 +0000

    Mon, Oct 7, 2024, 4:23 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -39,6 +39,11 @@ int main(int argc, char *argv[])
 
                 while (fread(buffer, sizeof(BYTE), HEADER_SIZE, f) != 0)
                 {
+                    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+                    {
+                        return 0;
+                    }
+
                     fwrite(buffer, sizeof(BYTE), HEADER_SIZE, nova_img);
                 }
             }
