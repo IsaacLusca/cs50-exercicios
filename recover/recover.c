50d501bd9fb534a23f0d951092b21f1ef7218845
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-07 16:40:01 +0000

    Mon, Oct 7, 2024, 1:40 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,10 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-
+    if (argv[1])
 }
 
 // obrir o card
 // procurar na memoria algum inicio de arquivo jpeg
-// se encontrado, deve abrir uma nova jpeg
+// se encontrado, deve abrir uma nova jpege gravara blocos de 512 bytes
+// 
 
