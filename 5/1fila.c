beb1bc9de1ba8b598b90d3538f6317b119bbcf30
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-08 17:18:18 +0000

    Tue, Oct 8, 2024, 2:18 PM -03:00

--- 5/1fila.c
+++ 5/1fila.c
@@ -1,1 +1,14 @@
-// fila 
+// fila
+// fifo. FIRST IN, FIRST OUT
+//  primeiro a entrar, primeiro a sair
+// enqueue e dequeue
+
+exemplo:
+
+const int CAPACITY = 50;
+
+typedef struct
+{
+    person people[CAPACITY];
+    int size;
+} queue;
