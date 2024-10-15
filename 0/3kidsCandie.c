8a727f54d49c874f5891b18a7ba78856a26e8aae
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 18:26:55 +0000

    Tue, Oct 15, 2024, 3:26 PM -03:00

--- 0/3kidsCandie.c
+++ 0/3kidsCandie.c
@@ -1,33 +1,27 @@
-
+#include <stdio.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
-    // tera um array de candies que cada criança possui
-    // tera um extraCandie que sera somado para cada criança
+// bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+//     // tera um array de candies que cada criança possui
+//     // tera um extraCandie que sera somado para cada criança
 
-}
+// }
 
 
-
-bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
-    // Alocar memória para o array de resultados
-    bool* result = (bool*)malloc(candiesSize * sizeof(bool));
-
-    // Encontrar o número máximo de balas entre as crianças
-    int maxCandies = 0;
-    for (int i = 0; i < candiesSize; i++) {
-        if (candies[i] > maxCandies) {
-            maxCandies = candies[i];
-        }
+int main()
+{
+    int n = 3;
+    int crianca[n];
+    crianca[0] = 2;
+    crianca[1] = 3;
+    crianca[2] = 5;
+    const int x = 10;
+    for (int i = 0; i < crianca[i]; i++)
+    {
+        crianca[i] += x;
+        printf("Criança %i: %i\n", i + 1, crianca[i]);
     }
 
-    // Verificar para cada criança se ela terá a maior quantidade de balas
-    for (int i = 0; i < candiesSize; i++) {
-        result[i] = (candies[i] + extraCandies >= maxCandies);
-    }
 
-    // Definir o tamanho do array de retorno
-    *returnSize = candiesSize;
-    return result;
 }
