b1e4660df5d6269292b0ea442202ed48755e790c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 15:11:15 +0000

    Tue, Oct 15, 2024, 12:11 PM -03:00

--- 0/leety_string.c
+++ 0/leety_string.c
@@ -4,25 +4,10 @@
 
 char * mergeAlternately(char * word1, char * word2)
 {
-    char *merged = malloc((strlen(word1) + strlen(word2) + 1) * sizeof(char));
-    int i = 0, j = 0, k = 0;
 
-    while (word1[i] != '\0' || word2[j] != '\0')
-    {
-        if (word1[i] != '\0')
-        {
-            merged[k] = word1[i];
-            k++;
-            i++;
-        }
-        if (word2[j] != '\0')
-        {
-            merged[k] = word2[j];
-            k++;
-            j++;
-        }
-    }
-    merged[k] = '\0';
-    printf("%s\n", merged);
-    return merged;
+}
+
+int main(void)
+{
+    char *merge = mergeAlternately(palavra1, palavra2);
 }
