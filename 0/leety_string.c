3ec20332855cfabc769446f1fd7751c42e953132
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 15:14:55 +0000

    Tue, Oct 15, 2024, 12:14 PM -03:00

--- 0/leety_string.c
+++ 0/leety_string.c
@@ -4,10 +4,15 @@
 
 char * mergeAlternately(char * word1, char * word2)
 {
-
+    char *merged =
 }
 
 int main(void)
 {
-    char *merge = mergeAlternately(palavra1, palavra2);
+    char *palavra1 = "abc";
+    char *palavra2 = "pqr";
+
+    char *mesclar = mergeAlternately(palavra1, palavra2);
+
+    printf("%s\n", mesclar);
 }
