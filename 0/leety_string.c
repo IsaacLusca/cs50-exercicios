dc9eec70a19cadbec70f7b21418c9d8fd3d62752
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 14:29:12 +0000

    Tue, Oct 15, 2024, 11:29 AM -03:00

---
+++ 0/leety_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char * mergeAlternately(char * word1, char * word2)
+{
+    char word1[100];
+    char word2[100];
+    char merged[200];
+    int i = 0, j = 0, k = 0;
+    scanf("%s", word1);
+    scanf("%s", word2);
+
+    while (word1[i] != '\0' || word2[j] != '\0')
+    {
+        if (word1[i] != '\0')
+        {
+            merged[k] = word1[i];
+            k++;
+            i++;
+        }
+        if (word2[j] != '\0')
+        {
+            merged[k] = word2[j];
+            k++;
+            j++;
+        }
+    }
+    printf("%s\n", merged);
+    return 0;
+}
