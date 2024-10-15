61f4bf13b5f8d9208c8d1379093dbfe4fe5c198c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 20:54:08 +0000

    Tue, Oct 15, 2024, 5:54 PM -03:00

--- 0/4flower.c
+++ 0/4flower.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+
+    bool *result = malloc(flowerbedSize * sizeof(bool));
+
+    for (int i = 1; i < flowerbedSize; i++)
+    {
+        if (flowerbed[i - 1] )
+    }
 
 }
