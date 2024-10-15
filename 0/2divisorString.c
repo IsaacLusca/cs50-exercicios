bbd725004e59463c5bc94c42ac0bba451c35c025
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 15:38:38 +0000

    Tue, Oct 15, 2024, 12:38 PM -03:00

---
+++ 0/2divisorString.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* gcdOfStrings(char* str1, char* str2)
+{
+    int comp1 = strlen(str1);
+    int comp2 = strlen(str2);
+
+    if (comp1 >= comp2)
+    {
+        char *divisor = malloc((comp1 + 1) * sizeof(char));
+    }
+    else
+    {
+        char *divisor = malloc((comp2 + 1) * sizeof(char));
+    }
+}
+
+int main()
+{
+    char *palavra1 = "ABC";
+    char *palavra2 = "ABCDEF";
+
+    char *divisor_comum = gcdOfStrings(palavra1, palavra2);
+    printf("Divisor comum: %s\n", divisor_comum);
+}
