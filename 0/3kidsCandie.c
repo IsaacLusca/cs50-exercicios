29aebdc394e2e93ae0183f97d59fde1f0318b5e8
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 18:36:50 +0000

    Tue, Oct 15, 2024, 3:36 PM -03:00

--- 0/3kidsCandie.c
+++ 0/3kidsCandie.c
@@ -3,38 +3,36 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-// bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
-//     // tera um array de candies que cada criança possui
-//     // tera um extraCandie que sera somado para cada criança
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+    // tera um array de candies que cada criança possui
+    // tera um extraCandie que sera somado para cada criança
+    int n;
+    candies[n];
 
-// }
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+}
 
 
-int main()
-{
+int main() {
     int n = 3;
     int crianca[n];
     crianca[0] = 2;
     crianca[1] = 3;
     crianca[2] = 5;
-
-
     const int x = 10;
     bool check[n];
-    for (int i = 0; i < crianca[i]; i++)
-    {
+
+    for (int i = 0; i < n; i++) {
         crianca[i] += x;
-        if ((crianca[i] += x) >= 13)
-        {
+        if (crianca[i] >= 13) {
             check[i] = true;
-            printf("Criança %i: %i true\n ", i + 1, crianca[i]);
-        }
-        else
-        {
+            printf("Criança %i: %i true\n", i + 1, crianca[i]);
+        } else {
             check[i] = false;
-            printf("Criança %i: %i false\n ", i + 1, crianca[i]);
+            printf("Criança %i: %i false\n", i + 1, crianca[i]);
         }
     }
-
-
 }
