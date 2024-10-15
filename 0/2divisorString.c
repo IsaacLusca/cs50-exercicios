12c69331e8b8f0adb7683ef21ad75bcbdd9495e6
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 17:16:19 +0000

    Tue, Oct 15, 2024, 2:16 PM -03:00

--- 0/2divisorString.c
+++ 0/2divisorString.c
@@ -5,11 +5,13 @@
 // função auxiliar para calcular gcd do comprimento.
 int gcd(int a, int b)
 {
-    if (b == 0)
+    while (b != 0)
     {
-        return a;
+        int temp = b;
+        b = a % b;
+        a = temp;
     }
-    return gcd(b, a % b);
+    return a;
 }
 
 char* gcdOfStrings(char* str1, char* str2)
