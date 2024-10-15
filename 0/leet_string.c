aad251e4db466db015f73fd7beb5798b26ec40ca
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 14:02:22 +0000

    Tue, Oct 15, 2024, 11:02 AM -03:00

--- 0/leet_string.c
+++ 0/leet_string.c
@@ -4,16 +4,16 @@
 int main(void)
 {
 
-    char word1;
+    char word1 = '\0';
     scanf("%s", word1);
-    char word2;
+    char word2 = '\0';
     scanf("%s", word2);
-    char merged;
+    char merged = '\0';
 
     for (int i = 0, n = strlen(word1), x = strlen(word2); i < n || i < x; i++)
     {
         merged[i] = word1[i] + word2[i];
     }
 
-    printf("%s", merged);
+    printf("%s", *merged);
 }
