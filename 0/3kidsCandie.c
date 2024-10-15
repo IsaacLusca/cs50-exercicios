a3cb6618c9821a1a8efd5d18ea72338fe7f22a4a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 20:19:16 +0000

    Tue, Oct 15, 2024, 5:19 PM -03:00

--- 0/3kidsCandie.c
+++ 0/3kidsCandie.c
@@ -1,38 +1,61 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
     // tera um array de candies que cada criança possui
     // tera um extraCandie que sera somado para cada criança
-    int n;
-    candies[n];
 
-    for (int i = 0; i < n; i++)
+    bool *result = malloc(candiesSize * sizeof(bool));
+
+    int maxCandies = candies[0];
+
+    for (int i = 0; i < candiesSize; i++)
     {
-        
+        maxCandies = (maxCandies > candies[i]) ? maxCandies: candies[i];
+        // if (candies[i] > maxCandies)
+        // {
+        //     maxCandies = candies[i];
+        // }
     }
+
+    for (int i = 0; i < candiesSize; i++)
+    {
+        int totalCandies = candies[i] + extraCandies;
+
+        result[i] = (totalCandies >= maxCandies) ? true:false;
+
+        // if (totalCandies >= maxCandies)
+        // {
+        //     result[i] = true;
+        // }
+        // else
+        // {
+        //     result[i] = false;
+        // }
+    }
+
+    *returnSize =  candiesSize;
+    return result;
 }
 
 
 int main() {
-    int n = 3;
-    int crianca[n];
-    crianca[0] = 2;
-    crianca[1] = 3;
-    crianca[2] = 5;
-    const int x = 10;
-    bool check[n];
+    int candies[] = {2, 3, 5, 1, 3};
+    int extraCandies = 3;
+    int returnSize;
 
-    for (int i = 0; i < n; i++) {
-        crianca[i] += x;
-        if (crianca[i] >= 13) {
-            check[i] = true;
-            printf("Criança %i: %i true\n", i + 1, crianca[i]);
-        } else {
-            check[i] = false;
-            printf("Criança %i: %i false\n", i + 1, crianca[i]);
-        }
+    // Chamada da função
+    bool* result = kidsWithCandies(candies, 5, extraCandies, &returnSize);
+
+    // Impressão do resultado
+    for (int i = 0; i < returnSize; i++) {
+        printf("Criança %i: %s\n", i + 1, result[i] ? "true" : "false");
     }
+
+    // Libera a memória alocada
+    free(result);
+    return 0;
 }
