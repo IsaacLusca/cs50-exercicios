57e7ac468b473c9f103ae3ed12f4dbc5303edd0c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 17:49:04 +0000

    Tue, Oct 15, 2024, 2:49 PM -03:00

--- 0/2divisorString.c
+++ 0/2divisorString.c
@@ -57,8 +57,8 @@ char *gcdOfStrings(char* str1, char* str2)
 
 int main()
 {
-    char palavra1[] = "LEET";
-    char palavra2[] = "CODE";
+    char palavra1[] = "ABCABC";
+    char palavra2[] = "AB";
 
     char *divisor_comum = gcdOfStrings(palavra1, palavra2);
     printf("Divisor comum: %s\n", divisor_comum);
