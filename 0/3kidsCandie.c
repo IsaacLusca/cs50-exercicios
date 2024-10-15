fe15595e0949d60258f0915c84e629097eb4bd20
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-15 18:18:52 +0000

    Tue, Oct 15, 2024, 3:18 PM -03:00

--- 0/3kidsCandie.c
+++ 0/3kidsCandie.c
@@ -1,12 +1,20 @@
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
     // tera um array de candies que cada criança possui
     // tera um extraCandie que sera somado para cada criança
-    int sum = 0;
     for (int i = 0; i < candies[i]; i++)
     {
-        sum = candie[i] + extracandies;
+        if ((candies[i] + extraCandies) > candiesSize)
+        {
+            returnSize[i] = true;
+        }
+        else
+        {
+            returnSize[i] = false;
+        }
     }
+    return returnSize;
 }
