76b84f1237ac6749766135a450b3d72c57ffc051
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-17 14:54:53 +0000

    Thu, Oct 17, 2024, 11:54 AM -03:00

--- 0/4flower.c
+++ 0/4flower.c
@@ -3,20 +3,16 @@
 #include <stdlib.h>
 
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    for (int i = 1; i < flowerbedSize; i++)
+    {
+        if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+        {
+            flowerbed[i] == 1;
+            n--;
 
-    for (int i = 0; i < flowerbedSize; i++) {
-        // Verifica se a posição atual está vazia
-        if (flowerbed[i] == 0) {
-            // Verifica se pode plantar na posição atual
-            if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbedSize - 1 || flowerbed[i + 1] == 0)) {
-                // Planta a flor
-                flowerbed[i] = 1;
-                n--;
-
-                // Se todas as flores foram plantadas, retorna true
-                if (n == 0) {
-                    return true;
-                }
+            if (n == 0)
+            {
+                return true;
             }
         }
     }
