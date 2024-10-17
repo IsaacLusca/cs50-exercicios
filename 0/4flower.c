d297ba334acf8729a6b5cc20499f158d65dd5da5
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-17 14:14:36 +0000

    Thu, Oct 17, 2024, 11:14 AM -03:00

--- 0/4flower.c
+++ 0/4flower.c
@@ -7,10 +7,6 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
     int checar[n];
     for (int i = 0; i < flowerbedSize; i++)
     {
-        if(i < 0)
-        {
-            continue;
-        }
         if (flowerbed[i] == 0)
         {
             if(flowerbed[i - 1] == 0 && flowerbed[i + i] == 0)
