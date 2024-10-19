34c3bb09903a4eff9c73836eb3dbf5417c2aed7d
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-19 18:14:39 +0000

    Sat, Oct 19, 2024, 3:14 PM -03:00

--- 0/7mergesorted.c
+++ 0/7mergesorted.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
+// {
+
+// }
+
 int main()
 {
     int nums1[] = {1, 2, 3, 0, 0, 0};
+    int nums2[] = {2, 5, 6};
 }
