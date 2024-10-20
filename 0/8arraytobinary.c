6bb798f4188ca0c0e6287baca12bc1233dd8dbbf
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-20 14:54:44 +0000

    Sun, Oct 20, 2024, 11:54 AM -03:00

--- 0/8arraytobinary.c
+++ 0/8arraytobinary.c
@@ -1,2 +1,18 @@
-// Estrutura de uma arvore
+#include <stdio.h>
+#include <stdlib.h>
 
+// Estrutura de uma arvore
+// Uma árvore vai ser dois ponteiros
+// Um para direita e esquerda.
+typedef struct no
+{
+    int valor;
+    struct no *direita;
+    struct no *esquerda;
+} noArv;
+
+
+int main()
+{
+    return 0;
+}
