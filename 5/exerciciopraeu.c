f3eb1836d9fc0305fd3dda01d0a6586b49bb8755
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-20 18:17:12 +0000

    Sun, Oct 20, 2024, 3:17 PM -03:00

--- 5/exerciciopraeu.c
+++ 5/exerciciopraeu.c
@@ -11,8 +11,13 @@ int main()
 {
     int lista[4] = {1, 2, 3, 4};
     no *atual = NULL;
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 4; i++)
     {
-        no *nova = malloc(sizeof(no));
+        if (i < 3)
+        {
+            no *nova = malloc(sizeof(no));
+            nova->valor = lista[i]
+            
+        }
     }
 }
