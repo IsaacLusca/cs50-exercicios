d7c093e9b8f2b460a88349f1197dd6da2e3b632d
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-24 20:41:22 +0000

    Thu, Oct 24, 2024, 5:41 PM -03:00

--- 6/blur.py
+++ 6/blur.py
@@ -1,5 +1,12 @@
 from PIL import Image, ImageFilter
 
+# aplicando embassadmento
 before = Image.open("arquivo.bmp)
 after = before.filter(ImageFilter.BoxBlur(10))
+after.save("saida.bmp")
+
+
+# Filtro de
+before = Image.open("bridge.bmp)
+after = before.filter(ImageFilter.FIND_EDGES)
 after.save("saida.bmp")
