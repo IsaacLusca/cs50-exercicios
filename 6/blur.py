e22f302413dea9b15fa0024f4c45409d2b879812
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-24 20:44:23 +0000

    Thu, Oct 24, 2024, 5:44 PM -03:00

--- 6/blur.py
+++ 6/blur.py
@@ -14,3 +14,6 @@ after.save("saida.bmp")
 # Em python, como tem varias bibliotecas, tem uma gama de funções
 # Como nesssa de detectar imagem
 # primeiro baixa com pip install face_recognition
+
+# no exemplo tem o elenco de ofice
+# e outra imagem com uma pessoa especifica
