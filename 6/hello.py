45fdd96af10e8b833c03cf8fcb80c09ba743221d
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-24 21:27:59 +0000

    Thu, Oct 24, 2024, 6:27 PM -03:00

--- 6/hello.py
+++ 6/hello.py
@@ -1,4 +1,11 @@
 from cs50 import get_string
 
-pergunta = input("What's your name? ")
-print(f"Hello, {pergunta}")
+#pergunta = input("What's your name? ")
+#print(f"Hello, {pergunta}")
+fatorial = input("fatorial de quanto? ")
+
+count = 0
+i = 0
+while (i < fatorial):
+    fatorial = fatorial * (fatorial - 1)
+    i++
