eed370b2317dcbe0e72ac73d987c031252f41dec
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-26 19:09:04 +0000

    Sat, Oct 26, 2024, 4:09 PM -03:00

--- 6/agree.py
+++ 6/agree.py
@@ -2,5 +2,13 @@ s = input("Você concorda?")
 
 # Diferente de c que usa ||, em python é or
 
-if s == "Y" or s == "y":
-    print("Concordo.")
+# if s == "Y" or s == "y":
+#     print("Concordo.")
+
+s = s.lower()
+
+while True:
+    if s in ["y", "yes"]:
+        print("concordo.")
+    elif s in ["n", "no"]:
+        print("nao concordo")
