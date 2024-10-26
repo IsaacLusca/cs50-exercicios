7cca8a3429605dcd7f36eb2fe720c5ccb099613a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-26 18:31:08 +0000

    Sat, Oct 26, 2024, 3:31 PM -03:00

--- 6/condicionais.py
+++ 6/condicionais.py
@@ -1,6 +1,12 @@
 from cs50 import get_int
 
-x = get_int(input("x: "))
-y = get_int(input("y "))
+x = get_int("x: ")
+y = get_int("y ")
 
-print(x + y)
+
+if x < y:
+    print("x é menor")
+elif x > y:
+    print("x é maior")
+else:
+    print("são inguais")
