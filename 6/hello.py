d3363a766b1780e617a6f39f06584e27d53b167c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-26 18:17:11 +0000

    Sat, Oct 26, 2024, 3:17 PM -03:00

--- 6/hello.py
+++ 6/hello.py
@@ -2,4 +2,13 @@ from cs50 import get_string
 
 #pergunta = input("What's your name? ")
 #print(f"Hello, {pergunta}")
-fatorial = int(input("fatorial de quanto? "))
+x = int(input("x: "))
+y = int(input("y "))
+
+print(x + y)
+
+# range
+# list
+# tuple
+# dict
+# set
