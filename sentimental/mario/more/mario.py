912a972fea5024ed767632bbcb9f3b2ce82d35dc
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-27 22:09:44 +0000

    Sun, Oct 27, 2024, 7:09 PM -03:00

---
+++ sentimental/mario/more/mario.py
@@ -0,0 +1,26 @@
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+
+def mario(n):
+    temp = n - 1
+    i = 1
+    while i <= n:
+        print(" " * temp + "#" * i + "  " + "#" * i)
+        i += 1
+        temp -= 1
+
+
+def main():
+    while True:
+        num = get_int("Height: ")
+        if 1 <= num <= 8:
+            mario(num)
+            break
+
+
+main()
