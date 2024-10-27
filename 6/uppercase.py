d8b81c504022ac32ec0f15223e07d46cc8260e5a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-27 17:21:05 +0000

    Sun, Oct 27, 2024, 2:21 PM -03:00

--- 6/uppercase.py
+++ 6/uppercase.py
@@ -1,4 +1,10 @@
-before = input("Digite seu texto: ")
-after = before.upper()
+# before = input("Digite seu texto: ")
+# after = before.upper()
 
-print(f"ficou: {after}")
+# print(f"ficou: {after}")
+
+before = input("Before: ")
+print("After: ", end="") #para evitar que tenha quebras de linhas para cada palavra:
+
+for c in before:
+    print(c.upper(), end="")
