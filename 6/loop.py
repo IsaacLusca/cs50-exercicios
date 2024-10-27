e516431f2e14c68621069b3a68435c4a52644c54
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-27 17:10:50 +0000

    Sun, Oct 27, 2024, 2:10 PM -03:00

--- 6/loop.py
+++ 6/loop.py
@@ -1,5 +1,11 @@
 i = 0
 
-while i < 3:
-    print("MIAU MIAUUUUUUUU")
-    i += 1
+# while i < 3:
+#     print("MIAU MIAUUUUUUUU")
+#     i += 1
+
+# achei o design de for pessimo do python...
+
+# no lugar da lista numerica [1, 2, 3]
+for i in range(3):
+    print("OLAAAAA")
