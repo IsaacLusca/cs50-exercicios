b1be2b60542946a5adb12b02ddf29041fbe926ce
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-28 14:59:02 +0000

    Mon, Oct 28, 2024, 11:59 AM -03:00

--- sentimental/cash/cash.py
+++ sentimental/cash/cash.py
@@ -5,79 +5,36 @@ B = 0.05
 C = 0.10
 D = 0.25
 
-def main():
 
-    while True:
-        valor = get_float("Numero: ")
-        if valor > 0:
-            break
+def calc(valor):
 
     moeda = 0
     while valor >= D:
         moeda += 1
-        valor -= D
+        valor = round(valor - D, 2)
 
     while valor >= C:
         moeda += 1
-        valor -= C
+        valor = round(valor - C, 2)
 
     while valor >= B:
         moeda += 1
-        valor -= B
+        valor = round(valor - B, 2)
 
     while valor >= A:
         moeda += 1
-        valor -= A
+        valor = round(valor - A, 2)
 
     print(f"{moeda}")
 
+
+def main():
+
+    while True:
+        cash = get_float("Numero: ")
+        if cash > 0:
+            calc(cash)
+            break
+
+
 main()
-
-
-# #include <cs50.h>
-# #include <stdio.h>
-
-# #define A 1
-# #define B 5
-# #define C 10
-# #define D 25
-
-# int main(void)
-# {
-#     int valor;
-#     int moeda = 0;
-#     int n;
-
-#     do
-#     {
-#         valor = get_int("Change owed: ");
-#     }
-#     while (valor < 1);
-
-#     // Algoritmo guloso, vai verificando do maior valor até o menor.
-#     while (valor >= D)
-#     {
-#         moeda++;
-#         valor -= D;
-#     }
-
-#     while (valor >= C)
-#     {
-#         moeda++;
-#         valor -= C;
-#     }
-
-#     while (valor >= B)
-#     {
-#         moeda++;
-#         valor -= B;
-#     }
-
-#     while (valor >= A)
-#     {
-#         moeda++;
-#         valor -= A;
-#     }
-
-#     printf("%i\n", moeda);
-# }
