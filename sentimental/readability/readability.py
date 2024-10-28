85eb99486e32eb9580ac086a84018cfa8b988dd6
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-28 15:43:32 +0000

    Mon, Oct 28, 2024, 12:43 PM -03:00

--- sentimental/readability/readability.py
+++ sentimental/readability/readability.py
@@ -13,6 +13,76 @@ def cont_sentencas(texto):
     sentenca = len(re.split(r'[.!?]+', texto)) - 1
 
 
+def grade(indice):
+        if (calculo >= 16)
+        {
+            printf("Grade 16+\n");
+        }
+        else if (calculo < 1)
+        {
+            printf("Before Grade 1\n");
+        }
+        else if (calculo >= 1 && calculo < 2)
+        {
+            printf("Grade 1\n");
+        }
+        else if (calculo >= 2 && calculo < 3)
+        {
+            printf("Grade 2\n");
+        }
+        else if (calculo >= 3 && calculo < 4)
+        {
+            printf("Grade 3\n");
+        }
+        else if (calculo >= 4 && calculo < 5)
+        {
+            printf("Grade 4\n");
+        }
+        else if (calculo >= 5 && calculo < 6)
+        {
+            printf("Grade 5\n");
+        }
+        else if (calculo >= 6 && calculo < 7)
+        {
+            printf("Grade 6\n");
+        }
+        else if (calculo >= 7 && calculo < 8)
+        {
+            printf("Grade 7\n");
+        }
+        else if (calculo >= 8 && calculo < 9)
+        {
+            printf("Grade 8\n");
+        }
+        else if (calculo >= 9 && calculo < 10)
+        {
+            printf("Grade 9\n");
+        }
+        else if (calculo >= 10 && calculo < 11)
+        {
+            printf("Grade 10\n");
+        }
+        else if (calculo >= 11 && calculo < 12)
+        {
+            printf("Grade 11\n");
+        }
+        else if (calculo >= 12 && calculo < 13)
+        {
+            printf("Grade 12\n");
+        }
+        else if (calculo >= 13 && calculo < 14)
+        {
+            printf("Grade 13\n");
+        }
+        else if (calculo >= 14 && calculo < 15)
+        {
+            printf("Grade 14\n");
+        }
+        else if (calculo >= 15 && calculo < 16)
+        {
+            printf("Grade 15\n");
+        }
+
 def main():
 
     texto = get_string("Text: ")
@@ -28,7 +98,7 @@ def main():
 
     indice = round(indice, 2)
 
-    
+
 main()
 
 
