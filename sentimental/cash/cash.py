3f2d0a982a71b034a26ff100dcf03adbede404a9
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-28 14:27:22 +0000

    Mon, Oct 28, 2024, 11:27 AM -03:00

--- sentimental/cash/cash.py
+++ sentimental/cash/cash.py
@@ -1,7 +1,58 @@
 from cs50 import get_float
 
+
+
 def main():
     n = get_float("Numero: ")
     print(n)
 
 main()
+
+
+# #include <cs50.h>
+# #include <stdio.h>
+
+# #define A 1
+# #define B 5
+# #define C 10
+# #define D 25
+
+# int main(void)
+# {
+#     int valor;
+#     int moeda = 0;
+#     int n;
+
+#     do
+#     {
+#         valor = get_int("Change owed: ");
+#     }
+#     while (valor < 1);
+
+#     // Algoritmo guloso, vai verificando do maior valor até o menor.
+#     while (valor >= D)
+#     {
+#         moeda++;
+#         valor -= D;
+#     }
+
+#     while (valor >= C)
+#     {
+#         moeda++;
+#         valor -= C;
+#     }
+
+#     while (valor >= B)
+#     {
+#         moeda++;
+#         valor -= B;
+#     }
+
+#     while (valor >= A)
+#     {
+#         moeda++;
+#         valor -= A;
+#     }
+
+#     printf("%i\n", moeda);
+# }
