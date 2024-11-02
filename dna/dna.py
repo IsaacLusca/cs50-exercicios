d02bd5e4e8a4e071edb4a695f7f2bfa8342ac6cc
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-02 18:55:26 +0000

    Sat, Nov 2, 2024, 3:55 PM -03:00

--- dna/dna.py
+++ dna/dna.py
@@ -23,40 +23,22 @@ def main():
         # print(sequence)
     # TODO: Find longest match of each STR in DNA sequence
 
-    sequences_big = ['AGATC', 'TTTTTTCT', 'AATG', 'TCTAG', 'GATA', 'TATC' ,'GAAA' ,'TCTG']
-    count_big = {}
-    sequences_small = ["AGATC", "AATG", "TATC"]
-    count_small = {}
+    sequences = ["AGATC", "AATG", "TATC"]
+    count = {}
     # for i in sequences:
     #     count[i] = sequence.count(i)
 
     # for sequence, occurrences in count.items():
     #     print(f"A sequência '{sequence}' aparece {occurrences} vezes no arquivo.")
 
-    for seq in sequences_big:
-        count_big[seq] = longest_match(sequence, seq)
-        print(count_big)
-    for seq in sequences_small:
-        count_small[seq] = longest_match(sequence, seq)
-        print(count_small)
+    for seq in sequences:
+        count[seq] = longest_match(sequence, seq)
+        # print(count)
 
     for row in rows:
-        if sys.argv[1] == "large.csv":
-            if (int(row["AGATC"]) == count_big["AGATC"] and
-                int(row["TTTTTTCT"]) == count_big["TTTTTTCT"] and
-                int(row["AATG"]) == count_big["AATG"] and
-                int(row["TCTAG"]) == count_big["TCTAG"] and
-                int(row["GATA"]) == count_big["GATA"] and
-                int(row["TATC"]) == count_big["TATC"] and
-                int(row["GAAA"]) == count_big["GAAA"] and
-                int(row["TCTG"]) == count_big["TCTG"]):
-                print(row["name"])
-                return
-        elif sys.argv[1] == "small.csv":
-            if (int(row["AGATC"]) == count_small["AGATC"] and int(row["AATG"]) == count_small["AATG"] and int(row["TATC"]) == count_small["TATC"]):
-                print(row["name"])
-                return
-
+        if (int(row["AGATC"]) == count["AGATC"] and int(row["AATG"]) == count["AATG"] and int(row["TATC"]) == count["TATC"]):
+            print(row["name"])
+            return
 
     print("No match\n")
 
