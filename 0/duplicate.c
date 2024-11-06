d4430b7ff6a2d82556d6c7c9e7d325f79703d853
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-06 00:35:30 +0000

    Tue, Nov 5, 2024, 9:35 PM -03:00

---
+++ 0/duplicate.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool containsDuplicate(int* nums, int numsSize) {
+    int temp = nums[0];
+    for (int i = 1; i < numsSize; i++)
+    {
+        if (temp == nums[i])
+        {
+            return true;
+        }
+        temp = nums[i];
+    }
+    return false;
+}
+
+int main()
+{
+    int nums = [1, 2, 3, 1]
+    int n = 4;
+
+    bool check = containsDUplicate(nums, 4);
+    if (check == true)
+    {
+        printf("True\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+}
