2eb6186a97e0e6e4bddb6f006538047ecdd10949
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-06 21:42:39 +0000

    Wed, Nov 6, 2024, 6:42 PM -03:00

--- movies/6.sql
+++ movies/6.sql
@@ -3,3 +3,6 @@ JOIN movies ON ratings.movie_id = movies.id
 WHERE movies.year = 2012;
 
 
+-- SELECT AVG(rating) FROM ratings WHERE movie_id IN (
+--     SELECT id FROM movies WHERE year = 2012
+-- );
