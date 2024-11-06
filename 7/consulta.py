a9bef9e79ff1ac567f9e88ba73bee9afba87270a
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-06 15:41:41 +0000

    Wed, Nov 6, 2024, 12:41 PM -03:00

--- 7/consulta.py
+++ 7/consulta.py
@@ -19,3 +19,13 @@ print(row["n"])
 # rows = db.execute("SELECT likes FROM posts WHERE id = ?", id);
 # likes = rows[0]["likes"]
 # db.execute("UPDATE posts SET likes = ? WHERE id = ?", likes + 1, id);
+
+# impedindo que certos valores não sejam computados
+
+# db.execute("BEGIN TRANSACTION")
+# rows = db.execute("SELECT likes FROM posts WHERE id = ?", id);
+# likes = rows[0]["likes"]
+# db.execute("UPDATE postes SET likes = ? WHERE id = ?", likes + 1, id);
+# db.execute("COMMIT")
+
+
