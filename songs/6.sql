d87526911b843bf34ee125a337175f1a2700bdcb
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-06 21:31:01 +0000

    Wed, Nov 6, 2024, 6:31 PM -03:00

--- songs/6.sql
+++ songs/6.sql
@@ -2,4 +2,5 @@
 --     SELECT id FROM artists WHERE name = 'Post Malone'
 -- );
 
-SELECT songs.name FROM songs JOIN artists ON songs.artist_id = artists.id WHERE artists.name = 'Post Malone';
+SELECT songs.name FROM songs
+JOIN artists ON songs.artist_id = artists.id WHERE artists.name = 'Post Malone';
