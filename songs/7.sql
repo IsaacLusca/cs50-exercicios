ad5db675d90bd2a25b21970e3ef7116890707a78
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-06 19:16:09 +0000

    Wed, Nov 6, 2024, 4:16 PM -03:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,3 +1,8 @@
 SELECT AVG(energy) FROM songs WHERE artist_id = (
     SELECT id FROM artists WHERE name = 'Drake'
 );
+
+-- SELECT AVG(songs.energy)
+-- FROM songs
+-- JOIN artists ON songs.artist_id = artists.id
+-- WHERE artists.name = 'Drake';
