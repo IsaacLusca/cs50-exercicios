07fe1717c852161c4fa0b7250a1596fdc48c8d3e
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-06 15:18:26 +0000

    Wed, Nov 6, 2024, 12:18 PM -03:00

--- 7/consulta.py
+++ 7/consulta.py
@@ -3,3 +3,8 @@ from cs50 import SQL
 
 # abrindo arquivos db:
 db = SQL("sqlite///favorites.db")
+
+favorite = input("Favorite: ")
+
+#linha
+rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
