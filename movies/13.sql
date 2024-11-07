f5a773598dba86b8613b1b21f83c821e5e24138b
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 19:36:10 +0000

    Thu, Nov 7, 2024, 4:36 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -9,6 +9,11 @@
 -- ) AND people.name != 'Kevin Bacon';
 
 
+SELECT DISTINCT(people.name) FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE people.name = 'Kevin Bacon' AND people.birth = 1958 AND people.name != 'Kevin Bacon';
+
 
 SELECT name FROM people WHERE id IN (
     SELECT person_id FROM stars WHERE movie_id IN (
