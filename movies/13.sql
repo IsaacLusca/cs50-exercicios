5dcbbe8d3a9d40c97183e141028589130989631c
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 19:26:27 +0000

    Thu, Nov 7, 2024, 4:26 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,3 +1,10 @@
-SELECT DISTINCT people.name FROM people
-JOIN stars ON stars.person_id = people.id
-JOIN movies ON movies.id = stars.person_id
+
+-- JOIN movies ON movies.id = stars.person_id
+
+SELECT name FROM people WHERE id IN (
+    SELECT person_id FROM stars WHERE movie_id IN (
+        SELECT movie_id FROM stars
+        JOIN people ON people.id = stars.person_id
+        WHERE people
+    )
+)
