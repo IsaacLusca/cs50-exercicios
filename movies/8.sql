b9ed984d1a5b39c25f34786206543d0a64a1ee29
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 17:43:03 +0000

    Thu, Nov 7, 2024, 2:43 PM -03:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,3 +2,9 @@ SELECT people.name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
 WHERE movies.title = 'Toy Story';
+
+-- SELECT name FROM people WHERE id IN(
+--     SELECT person_id FROM stars WHERE movie_id = (
+--         SELECT id FROM movies WHERE title = 'Toy Story'
+--     )
+-- );
