88bde71f5ac8c77cf3f13b2488396a1641f926bc
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 17:18:42 +0000

    Thu, Nov 7, 2024, 2:18 PM -03:00

--- movies/7.sql
+++ movies/7.sql
@@ -2,6 +2,6 @@ SELECT title, ratings.rating FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
 WHERE movies.year = 2010 ORDER BY title DESC;
 
-SELECT title,
-    (SELECT rating FROM ratings WHERE ratings.movie_id = movies.id) AS rating
-FROM movies WHERE movies.year = 2010 ORDER BY title DESC;
+-- SELECT title,
+--     (SELECT rating FROM ratings WHERE ratings.movie_id = movies.id) AS rating
+-- FROM movies WHERE movies.year = 2010 ORDER BY title DESC;
