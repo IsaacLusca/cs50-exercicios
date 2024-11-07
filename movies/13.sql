ef60d5f004b427b775de1a858df98b60902689a0
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 19:28:20 +0000

    Thu, Nov 7, 2024, 4:28 PM -03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,11 +1,11 @@
+SELECT 
 
--- JOIN movies ON movies.id = stars.person_id
 
-SELECT name FROM people WHERE id IN (
-    SELECT person_id FROM stars WHERE movie_id IN (
-        SELECT movie_id FROM stars
-        JOIN people ON people.id = stars.person_id
-        WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
-    )
-)
-AND name != 'Kevin Bacon';
+-- SELECT name FROM people WHERE id IN (
+--     SELECT person_id FROM stars WHERE movie_id IN (
+--         SELECT movie_id FROM stars
+--         JOIN people ON people.id = stars.person_id
+--         WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
+--     )
+-- )
+-- AND name != 'Kevin Bacon';
