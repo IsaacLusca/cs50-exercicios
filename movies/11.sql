3de7169b18d8533db725bd79ee0e81ed2648a1ac
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 18:52:46 +0000

    Thu, Nov 7, 2024, 3:52 PM -03:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,4 +2,4 @@ SELECT movies.title from movies
 JOIN directors ON directors.movie_id = movies.id
 JOIN people ON people.id = directors.person_id
 JOIN ratings ON ratings.movie_id = movies.id
-WHERE people.name = 'Chadwick Boseman';
+WHERE people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
