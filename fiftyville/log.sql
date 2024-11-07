28405681ee81abb4f493f1958ddd86bae247b246
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 20:42:38 +0000

    Thu, Nov 7, 2024, 5:42 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,3 +6,11 @@ FROM crime_scene_reports
 WHERE year = 2023
     AND month = 7
     AND day = 28;
+
+-- +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+-- | Vandalism took place at 12:04. No known witnesses.                                                                                                                                                                       |
+-- | Shoplifting took place at 03:01. Two people witnessed the event.                                                                                                                                                         |
+-- | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
+-- | Money laundering took place at 20:30. No known witnesses.                                                                                                                                                                |
+-- | Littering took place at 16:36. No known witnesses.                                                                                                                                                                       |
+-- +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
