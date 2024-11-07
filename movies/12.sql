3505b8ae275bb1dc384ace9424d94dc463a5b310
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-07 19:08:17 +0000

    Thu, Nov 7, 2024, 4:08 PM -03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,9 @@
-SELECT title FROM movies
-JOIN stars ON stars.movie_id = movies.id
-JOIN people ON people.id = stars.person_id
-WHERE people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence';
+SELECT title FROM movies WHERE id IN (
+    SELECT movie_id FROM stars
+    JOIN people ON people.id = stars.person_id WHERE people.name = 'Bradley Cooper'
+)
+AND id IN (
+    SELECT movie_id FROM stars
+    JOIN people ON people.id = stars.person_id WHERE people.name = 'Jennifer Lawrence'
+);
+
