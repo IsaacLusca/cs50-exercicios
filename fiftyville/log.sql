0c5a48dfa117e08fe0f5eab3991e040c8fba4514
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-11-12 17:25:27 +0000

    Tue, Nov 12, 2024, 2:25 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -61,3 +61,18 @@ WHERE name = 'Ruth' OR name = 'Eugene' OR name = 'Raymond';
 -- | 430845 | Ruth    | (772) 555-5770 | NULL            | HZB4129       |
 -- | 937274 | Raymond | (125) 555-8030 | NULL            | Y18DLY3       |
 -- +--------+---------+----------------+-----------------+---------------+
+
+SELECT name, transcript
+FROM interviews
+WHERE name = 'Ruth'
+    OR name = 'Eugene'
+    OR name = 'Raymond';
+
+-- +---------+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+-- | Raymond | “Good-night, Mister Sherlock Holmes.”                                                                                                                                                                                                                                                                               |
+-- | Ruth    | “I will get her to show me.”                                                                                                                                                                                                                                                                                        |
+-- | Eugene  | “I suppose,” said Holmes, “that when Mr. Windibank came back from France he was very annoyed at your having gone to the ball.”                                                                                                                                                                                      |
+-- | Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+-- | Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
+-- | Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+-- +---------+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
