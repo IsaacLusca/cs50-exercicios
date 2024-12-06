a4002c7f4cce4f1ff8cf965cf0f090883c68feb1
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-12-06 17:53:52 +0000

    Fri, Dec 6, 2024, 2:53 PM -03:00

---
+++ 0/teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fatorial(int n){
+    if (n == 0 || n == 1){
+        return 1;
+    }
+    return n * fatorial(n - 1);
+}
+
+int main(){
+    int num = 5;
+    printf("%i", fatorial(num));
+}
