7af49bed39dc34d0edea5cb3782d696516ecd865
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-12-06 18:09:32 +0000

    Fri, Dec 6, 2024, 3:09 PM -03:00

--- 0/teste.c
+++ 0/teste.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void tabelaAdicao(int n) {
-    printf("Tabela Z_%d para adição modular:\n", n);
+void t_mt(int n) {
+    printf("Tabela Z_%d para multiplicação modular:\n", n);
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            printf("%d ", (i + j) % n);
+            printf("%d ", (i * j) % n);
         }
         printf("\n");
     }
 }
 
-int main(){
+int main() {
     int n = 5;
-    tabelaAdicao(n);
+    t_mt(n);
 }
