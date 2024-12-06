9243a89b6d7fd6a9e26f793c9c354c1e58ac7b50
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-12-06 17:58:57 +0000

    Fri, Dec 6, 2024, 2:58 PM -03:00

--- 0/teste.c
+++ 0/teste.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int fatorial(int n){
-    if (n == 0 || n == 1){
+int fibonacci(int n) {
+    if (n == 0){
+        return 0;
+    }
+    if (n == 1){
         return 1;
     }
-    return n * fatorial(n - 1);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main(){
-    int num = 5;
-    printf("%i\n", fatorial(num));
+int main() {
+    int num = 10;
+    for (int i = 0; i < num; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
 }
+
+
