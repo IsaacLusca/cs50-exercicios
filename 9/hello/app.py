7f9bcd0fe75a4c676471ba2ed90b7c1afd329447
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-12-10 15:28:00 +0000

    Tue, Dec 10, 2024, 12:28 PM -03:00

--- 9/hello/app.py
+++ 9/hello/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/greet", methods=["GET"])
+@app.route("/greet", methods=["POST"])
 def greet():
     name = request.form.get("name", "world")
     return render_template("greet.html", name=name)
